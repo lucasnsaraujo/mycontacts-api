d8ddc7ba75244e6ff2798e32657d716adc530bc8
Lucas Nascimento <lucasnsaraujo0310@gmail.com>
2022-11-22 19:12:17 -0300

    ajuste

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -4,8 +4,7 @@ class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
     const rows = await db.query(`
-    SELECT *
-    FROM contacts
+    SELECT contacts.*, categories.name AS category_name FROM contacts
     JOIN categories ON categories.id = contacts.category_id
     ORDER BY contacts.name ${direction}
     `);
